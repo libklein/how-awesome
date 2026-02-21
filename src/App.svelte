afa439aa491f6709770c78f1f433038c83566c9b
Patrick Sean Klein <patrick@libklein.com>
2026-02-21 23:30:16 +0100

    Improved repo selector

--- src/App.svelte
+++ src/App.svelte
@@ -356,25 +356,30 @@
 <main>
     <div id="page-container">
         <section class="repo-selector-box" hidden={hasLoadedList}>
-            <label class="repo-label" for="awesome-repo-input">
-                <svg
-                    aria-hidden="true"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    width="16"
-                    data-view-component="true"
-                    class="repo-label-icon"
-                >
-                    <path
-                        d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"
-                    ></path>
-                </svg>
-                <span class="repo-label-body"> Repository </span>
-            </label>
-            <p class="repo-help">
-                Enter a GitHub awesome list repository URL or owner/repo path.
-            </p>
+            <header class="repo-selector-header">
+                <span class="repo-selector-header-logo">
+                    <AwesomeLogo />
+                </span>
+                <div class="repo-selector-header-body">
+                    <span
+                        ><a href="https://github.com/libklein">libklein</a>
+                        published
+                        <a>How Awesome?</a></span
+                    >
+                    <span>just now</span>
+                </div>
+            </header>
+            <section class="repo-help-container">
+                <p class="repo-help">
+                    How awesome automatically annotates GitHub awesome lists
+                    with repository statistics like the number of stars, last
+                    commit date, and more.
+                </p>
+                <p class="repo-help">
+                    Getting started is as easy as entering the URL of an awesome
+                    list or the owner/repo path.
+                </p>
+            </section>
             <form
                 class="repo-selector"
                 onsubmit={event => {
