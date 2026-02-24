e736e922a5aa8eddba2cee0977f603281557478f
Patrick Sean Klein <patrick@libklein.com>
2026-02-24 21:42:56 +0100

    Setup cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [svelte(), cloudflare()],
     resolve: {
         alias: {
             $lib: 'src/lib',
